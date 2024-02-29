76b33bc5350103a49caadb5d7d059470521833bc
HààMódY <143948816+byemuhvmed@users.noreply.github.com>
2024-02-29 05:36:25 +0200

    Add files via upload

---
+++ FallenMusic/Helpers/dossier.py
@@ -0,0 +1,62 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from FallenMusic import BOT_NAME
+
+PM_START_TEXT = """
+-› لاتفوتك مميزات السورس الجديد والأول 
+
+-› يدعم تشغيل الملفات الصوتية في المجموعات
+
+"""
+
+START_TEXT = """
+-› لاتفوتك مميزات السورس الجديد والأول 
+
+-› يدعم تشغيل الملفات الصوتية في المجموعات
+
+"""
+
+HELP_TEXT = f"""~ قائمة أوامر المستخدمين .
+
+- تشغيل  ~ لتشغيل ملف صوتي في المجموعة 
+- ايقاف مؤقت ~ لإيقاف الملف الصوتي مؤقتا
+- ايقاف ~ انهاء تشغيل الملف الصوتي
+- تخطي ~ تخطي الملف الصوتي الحالي
+- يوت ~ بحث من اليوتيوب وتنزيل
+- بنج ~ اضهار بنج البوت 
+
+
+"""
+
+HELP_SUDO = f"""- قائمة اوامر المطورين .
+
+- رفع مطور ~ ارفع مطور في البوت
+- السرعة ~ كشف سرعة البوت الحقيقية
+- المطور ~ اظهار مطور البوت المستخدم
+- السورس ~ اضهار سورس البوت الحالي
+- المطورين ~ اضهار مطورين البوت الحاليين
+- تك ~ تنزيل مطور من البوت الحالي
+- اذاعة ~ عمل اذاعة الى المجموعات 
+."""
+
+HELP_DEV = f""" اتركها فارغة """
---
+++ FallenMusic/Modules/تطوير.py
@@ -0,0 +1,37 @@
+import asyncio
+import random
+import os
+import time
+import requests
+from random import  choice, randint
+from pyrogram import Client, filters
+from FallenMusic.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from FallenMusic import app
+
+
+@app.on_message(
+    command(["سورس","السورس","المطور"])
+    & filters.group
+)
+async def maker(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/6fe7de7485c993122bc4a.jpg",
+        caption=f"""Developer Name : hamody . 
+Devloper Username : @MH_BP ، 
+‏ⓘ متسالنيش عن احوالي انا تايه .""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        " مطور السورس .", url=f"https://t.me/MH_BP"),
+                ],
+                [
+                   InlineKeyboardButton(
+                        " قناة المطور ", url=f"https://t.me/M_C_II"),
+                ],       
+            ]
+        ),
+    )
+
+
